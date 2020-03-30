87b44daf42464f9010ed2cb39df7a4b78990909d
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-30 19:51:23 +0900

    Add：Viewをインポート

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import android.view.View
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
