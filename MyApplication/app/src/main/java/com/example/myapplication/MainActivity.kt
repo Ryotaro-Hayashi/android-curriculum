23fbb6800c6fc26a7d22416174439647270a9b57
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-03-30 17:47:52 +0900

    Add：表示する画像の配列と、どの画像を表示しているかを管理するためのメンバ変数を追加

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    //スライドショーで表示する画像の配列
+    private val PHOTO_RES_IDS = intArrayOf(R.drawable.italy, R.drawable.london, R.drawable.paris)
+
+    //今どの画像を表示しているかを管理するためのメンバ変数
+    private var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        
+
     }
 }
