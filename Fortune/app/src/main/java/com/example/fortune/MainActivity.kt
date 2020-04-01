90f8d161701630e2db3f8d1adfd035d230361815
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-01 18:05:52 +0900

    Add：おみくじの結果画像を格納した配列を追加

--- Fortune/app/src/main/java/com/example/fortune/MainActivity.kt
+++ Fortune/app/src/main/java/com/example/fortune/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    // おみくじ結果画像の配列
+    private val FORTUNE_IMAGES = intArrayOf(R.drawable.omikuji_daikichi,
+        R.drawable.omikuji_chuukichi, R.drawable.omikuji_kichi,R.drawable.omikuji_syoukichi,
+        R.drawable.omikuji_suekichi,  R.drawable.omikuji_kyou, R.drawable.omikuji_daikyou)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
