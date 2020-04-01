ce16a67ac84bbf030ecc33adda767ee8133f9501
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-01 10:23:58 +0900

    Add：画面遷移の際に値を渡すための変数を設定

--- MyApplication/app/src/main/java/com/example/myapplication/PhotoDetailActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/PhotoDetailActivity.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 
 class PhotoDetailActivity : AppCompatActivity() {
 
+    //画面遷移の際に値を渡すための変数
+    val EXTRA_PHOTO_RES_ID = "EXTRA_PHOTO_RES_ID"
+    val EXTRA_DESCRIPTION_STRING = "EXTRA_DESCRIPTION_STRING"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_detail)
