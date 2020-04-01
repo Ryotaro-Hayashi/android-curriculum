60f459121ed7e15ccdc758b8d51454d923d22bf0
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-01 10:14:24 +0900

    Add：新しい画面を作成するために新規activityを追加

---
+++ MyApplication/app/src/main/java/com/example/myapplication/PhotoDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PhotoDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photo_detail)
+    }
+}
