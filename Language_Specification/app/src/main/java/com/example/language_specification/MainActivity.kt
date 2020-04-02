7805ebf401f368f663e5a6baed70cdd0653706d8
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-02 10:01:33 +0900

    Add：subActivityでクラスを作成して、MainActivityでインスタンスを作成

--- Language_Specification/app/src/main/java/com/example/language_specification/MainActivity.kt
+++ Language_Specification/app/src/main/java/com/example/language_specification/MainActivity.kt
@@ -8,5 +8,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // インスタンスを作成
+        val car = Car()
+        car.name()
     }
 }
---
+++ Language_Specification/app/src/main/java/com/example/language_specification/subActivity.kt
@@ -0,0 +1,6 @@
+package com.example.language_specification
+
+class Car() {
+    // メソッドを定義
+    fun name() = println("軽トラック")
+}
