13f91c8c2d0d0f424825dfc81d5096cb74ef7b7c
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-02 10:09:53 +0900

    Add：親クラス、子クラスを定義して、子クラスは親クラスを継承

--- Language_Specification/app/src/main/java/com/example/language_specification/subActivity.kt
+++ Language_Specification/app/src/main/java/com/example/language_specification/subActivity.kt
@@ -1,6 +1,35 @@
 package com.example.language_specification
 
-class Car() {
-    // メソッドを定義
-    fun name() = println("軽トラック")
+//class Car() {
+//    // メソッドを定義
+//    fun name() = println("軽トラック")
+//}
+
+// 親クラス
+open class Vehicle {
+    val speed = "走る"
+    val tire= "タイヤ"
+    val engine = "エンジン"
+    val seat = "シート"
+}
+
+// 親クラスを継承した子クラス
+class Car : Vehicle() {
+    fun car() {
+        println("親から受け継いだロジック")
+        println(speed)
+        println(tire)
+        println(engine)
+        println(seat)
+    }
+}
+
+class Bike : Vehicle() {
+    fun bike() {
+        println("親から受け継いだロジック")
+        println(speed)
+        println(tire)
+        println(engine)
+        println(seat)
+    }
 }
