77da1941ddccee23d326086835a43e59ab09133e
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-03 18:01:23 +0900

    Add：音楽とボタンを関連付けるためのクラスと音楽ファイルを再生できるようにインスタンスを作成

--- Piano/app/src/main/java/com/example/piano/MainActivity.kt
+++ Piano/app/src/main/java/com/example/piano/MainActivity.kt
@@ -3,7 +3,15 @@ package com.example.piano
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import kotlinx.android.synthetic.main.activity_main.*
+
 class MainActivity : AppCompatActivity() {
+
+    // 白鍵・黒鍵と音楽ファイルを対応づけるためのKeyデータクラスを定義
+    data class Key(val button: Button, val resource: Int)
+
+    // ボタンが押された時に最大10個まで音楽ファイルを同時に再生できるようにインスタンスを生成
+    private val soundPool = SoundPool.Builder().setMaxStreams(10).build()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
