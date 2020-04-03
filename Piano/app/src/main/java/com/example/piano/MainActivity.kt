070b5dca2f63f900bdfcebae21c9849b3e37c99f
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-03 18:05:06 +0900

    Add：ボタンと音楽ファイルを対応させたリストを追加

--- Piano/app/src/main/java/com/example/piano/MainActivity.kt
+++ Piano/app/src/main/java/com/example/piano/MainActivity.kt
@@ -16,5 +16,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 作成したKeyクラスを使って、各ボタンと音楽ファイルを対応させたリストを作成
+        val piano = listOf(
+            Key(white_0, R.raw.piano_c),
+            Key(black_0, R.raw.piano_c_sharp),
+            Key(white_1, R.raw.piano_d),
+            Key(black_1, R.raw.piano_d_sharp),
+            Key(white_2, R.raw.piano_e),
+            Key(white_3, R.raw.piano_f),
+            Key(black_2, R.raw.piano_f_sharp),
+            Key(white_4, R.raw.piano_g),
+            Key(black_3, R.raw.piano_g_sharp),
+            Key(white_5, R.raw.piano_a),
+            Key(black_4, R.raw.piano_a_sharp),
+            Key(white_6, R.raw.piano_b),
+            Key(white_7, R.raw.piano_c_octave))
     }
 }
