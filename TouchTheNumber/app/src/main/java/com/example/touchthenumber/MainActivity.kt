fb8a596cf4887c819270567f7d0aa91a263de3bd
林　遼太朗 <hys.rtr.forit@gmail.com>
2020-04-03 12:32:31 +0900

    Add：GameActivityへの画面遷移

--- TouchTheNumber/app/src/main/java/com/example/touchthenumber/MainActivity.kt
+++ TouchTheNumber/app/src/main/java/com/example/touchthenumber/MainActivity.kt
@@ -3,10 +3,20 @@ package com.example.touchthenumber
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import kotlinx.android.synthetic.main.activity_main.*
+// 画面遷移するためにインポート
+import android.content.Intent
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // GameActivityへの画面遷移
+        startButton.setOnClickListener{
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
